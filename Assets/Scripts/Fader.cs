bff8c99c160aa4f089f41db111536b90fe7fda39
pkrywulko <pkrywulko@gmail.com>
2020-11-27 19:14:38 +0100

    Create Fader for fading in and out when level starts or ends

---
+++ Assets/Scripts/Fader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Fader : PersistentSingleton<Fader>
+{
+    private CanvasGroup _canvasGroup;
+
+    private new void Awake()
+    {
+        base.Awake();
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public IEnumerator FadeOut(float time)
+    {
+        while (_canvasGroup.alpha < 1)
+        {
+            _canvasGroup.alpha += Time.deltaTime / time;
+            yield return null;
+        }
+    }
+    
+    public IEnumerator FadeIn(float time)
+    {
+        while (_canvasGroup.alpha > 0)
+        {
+            _canvasGroup.alpha -= Time.deltaTime / time;
+            yield return null;
+        }
+    }
+}
