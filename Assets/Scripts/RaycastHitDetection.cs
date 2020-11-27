9cbdad30c749b78bbcb1e812d8a86dac1d9773c2
pkrywulko <pkrywulko@gmail.com>
2020-11-27 18:41:14 +0100

    Implement playing hit sound on character death

--- Assets/Scripts/RaycastHitDetection.cs
+++ Assets/Scripts/RaycastHitDetection.cs
@@ -2,15 +2,18 @@
 using UnityEngine;
 
 [RequireComponent(typeof(RagdollController))]
+[RequireComponent(typeof(SfxPlayer))]
 public class RaycastHitDetection : MonoBehaviour
 {
     private bool _isHit = false;
     private bool _isTargeted = false;
     private RagdollController _ragdollController;
+    private SfxPlayer _sfxPlayer;
 
     private void Awake()
     {
         _ragdollController = GetComponent<RagdollController>();
+        _sfxPlayer = GetComponent<SfxPlayer>();
     }
 
     public void OnRaycastHit()
@@ -38,6 +41,7 @@ public class RaycastHitDetection : MonoBehaviour
         if (_isTargeted && !_isHit)
         {
             _isHit = true;
+            _sfxPlayer.PlayRandomSfx();
             _ragdollController.EnableRagdollEffect();
         }
     }
---
+++ Assets/Scripts/SfxPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SfxPlayer : MonoBehaviour
+{
+    [SerializeField] private ScreamHitAudioClipListSo audioClips;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayRandomSfx()
+    {
+        if (_audioSource.isPlaying) return;
+
+        var randomClip = audioClips.list[Random.Range(0, audioClips.list.Count)];
+        _audioSource.PlayOneShot(randomClip);
+    }
+}
