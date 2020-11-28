3d4dbfe7246903790f726e1fac068cfefa3c9f3f
pkrywulko <pkrywulko@gmail.com>
2020-11-28 15:45:16 +0100

    Add LevelUI prefab with component
    It shows message if you win / lose.
    Reload level function.
    Back to Main Menu function.

---
+++ Assets/Scripts/LevelUI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class LevelUI : MonoBehaviour
+{
+    [SerializeField] private Color winColor;
+    [SerializeField] private Color loseColor;
+
+    private TextMeshProUGUI _mainText;
+    private TextMeshPro _mainTmPro;
+    private const string WinString = "YOU WIN !";
+    private const string LoseString = "YOU LOSE !";
+
+    private void Awake()
+    {
+        _mainText = transform.Find("Canvas/Main Text").GetComponent<TextMeshProUGUI>();
+        _mainTmPro = transform.Find("Canvas/Main Text").GetComponent<TextMeshPro>();
+    }
+
+    public void ReloadLevel() //Attached to the button in Hierarchy
+    {
+        LevelLoader.Instance.ReloadLevel();
+    }
+
+    public void ReturnToMainMenu() //Attached to the button in Hierarchy
+    {
+        LevelLoader.Instance.LoadLevel(0);
+    }
+
+    private void LevelManagerOnLose(object sender, EventArgs e)
+    {
+        _mainText.SetText($"<color=\"red\">{LoseString}</color>");
+        _mainText.ForceMeshUpdate(true);
+    }
+
+    private void LevelManagerOnWin(object sender, EventArgs e)
+    {
+        _mainText.SetText($"<color=\"green\">{WinString}</color>");
+        _mainText.ForceMeshUpdate(true);
+    }
+    
+    private void OnEnable()
+    {
+        _mainText.text = string.Empty;
+        LevelManager.OnWin += LevelManagerOnWin;
+        LevelManager.OnLose += LevelManagerOnLose;
+    }
+
+    private void OnDisable()
+    {
+        LevelManager.OnWin -= LevelManagerOnWin;
+        LevelManager.OnLose -= LevelManagerOnLose;
+    }
+}
