71277a0ab22dc4bab110bef05234ceb15cc46a8d
pkrywulko <pkrywulko@gmail.com>
2020-12-09 22:55:09 +0100

    Add preview button in the Inspector for Audio Player scriptable objects

---
+++ Assets/Scriptable Objects/Editor/AudioPlayerEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AudioPlayerSo), true)]
+public class AudioPlayerEditor : Editor
+{
+    [SerializeField] private AudioSource previewer;
+
+    private void OnEnable()
+    {
+        previewer = EditorUtility
+            .CreateGameObjectWithHideFlags("Audio preview", HideFlags.HideAndDontSave, typeof(AudioSource))
+            .GetComponent<AudioSource>();
+    }
+
+    private void OnDisable()
+    {
+        DestroyImmediate(previewer.gameObject);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
+        if (GUILayout.Button("Preview"))
+        {
+            ((AudioPlayerSo)target).Play(previewer);
+        }
+        EditorGUI.EndDisabledGroup();
+    }
+}
